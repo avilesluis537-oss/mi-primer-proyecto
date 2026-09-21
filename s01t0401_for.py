d227240306ea63b9ba831b3700a52b3c27a6c4a8
avilesluis537-oss <avilesluis537@gmail.com>
2026-09-21 08:53:03 -0600

    🛜s01t0401 for

---
+++ s01t0401_for.py
@@ -0,0 +1,14 @@
+"""
+Escribir un que calcule la suma 
+de los "N" numeros naturales
+por ejemplo si n=100,el programa 
+calculara la suma del 1 al 100
+"""
+import time 
+#maraca de tiempo 
+timestamp_01 = time.time()
+n = 100
+# ciclo for
+for number in range (1,n+1):
+    print(str(number) + "I")
+
