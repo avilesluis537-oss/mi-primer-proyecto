2bdf5b6ede8c84db6cdc3ee937945ee9ea80abfa
avilesluis537-oss <avilesluis537@gmail.com>
2026-09-22 10:41:05 -0600

    ⭐s01t0401for Finaliza toma de tiempo de la suma de 100 numeros usando un for

--- s01t0401_for.py
+++ s01t0401_for.py
@@ -8,7 +8,23 @@ import time
 #maraca de tiempo 
 timestamp_01 = time.time()
 n = 100
+total_sum = 0
 # ciclo for
 for number in range (1,n+1):
-    print(str(number) + "I")
+    total_sum =  total_sum + number
+    #1: linea de codigo <- 0+1
+    #suma = 1
+    #2: sum<- 1 + 2
+    #suma = 3
+print(f"La suma de 1 hasta {n} es : {total_sum}")
 
+#Tomando el tiempo final
+timestamp_02 =  time.time()
+
+#Tomando el timepo del tiempo de ejecuicion
+print(f"Timepo de ejecicion: {timestamp_02 - timestamp_01} segundos")
+#Tomando el tiempo final
+timestamp_02 =  time.time()
+
+#Tomando el timepo del tiempo de ejecuicion
+print(f"Timepo de ejecicion: {(timestamp_02 - timestamp_01) * 1e6:.2f}  µs")
